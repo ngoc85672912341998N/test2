e73a460887508a7f60cce1fa73fdb7e9e75d9221
ngoc85672912341998N <92494149+ngoc85672912341998N@users.noreply.github.com>
2023-10-12 20:37:51 +0700

    Update Chat_page.jsx

--- src/components/Chat_page.jsx
+++ src/components/Chat_page.jsx
@@ -86,7 +86,7 @@ useEffect(() => {
 }, []);
   return (
     <Row className='w-100'>
-      <Col style={{ backgroundColor: '#252c48', height: '730px' }} sm={2}>
+      <Col style={{ backgroundColor: '#252c48', height: '670px' }} sm={2}>
         <Row className='mt-2'>
           <Col sm={1}></Col>
           <Col sm={10}>
